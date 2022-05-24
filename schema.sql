9f1caf72d0e4e280883b2eac16850d92d58ef916
SPLT19 <drsplt19@gmail.com>
2022-05-23 23:11:46 -0500

    debugging errors for Import. Some csv data was corrupted, and tables structure incorrect. Main structure debugged, analysis with queries started. First table with retirment information exported to a csv file. Saving changes.Saved query in external text program.  Current status: Queryes working

--- schema.sql
+++ schema.sql
@@ -58,6 +58,109 @@ CREATE TABLE dept_emp (
    PRIMARY KEY (dept_no)
 );
 
-
+--Debugging corrupted tables (import function issue)
 --Querry for confirmation
 SELECT * FROM departments; 
+
+
+---Recreate titles table
+CREATE TABLE titles (
+emp_no INT NOT NULL, 
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, from_date)
+);
+
+-- Select 
+SELECT * FROM dept_manager 
+
+-- Recreat dept_manager
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+--Select Queryes
+SELECT * FROM dept_manager 
+SELECT * FROM dept_emp
+SELECT * FROM employees
+SELECT * FROM salaries
+SELECT * FROM titles
+SELECT * FROM departments
+
+-- Recreat dept_emp
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+--ANALYSIS QUERYS--
+
+--Analysis  January 1, 1952, and December 31, 1955
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';	
+--(RESULTS messages: 90398 rows affected.)
+
+--Analysis  only  employees born in 1952 
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31'; 
+--( RESULT messages: 21209 rows affected.)
+
+--Analysis  only  employees born in 1953 
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+--( RESULT messages: 22857  rows affected.)
+
+--Analysis  only  employees born in 1954 
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+--( RESULT messages: 23228 rows affected.)
+
+--Analysis  only  employees born in 1955 
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+--( RESULT messages: 23104 rows affected.)
+
+--Narrow  Search for Retirement Eligibility (birth dates + hire date) 
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+--(RESULTS messages 1380 rows affected)
+
+
+--Counting querys
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+--(RESULTS from count: 41,380)
+
+
+--Create CSV Tables from querys
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+--(Result: Query returned successfully)
+
+--Inspect result
+SELECT * FROM retirement_info
